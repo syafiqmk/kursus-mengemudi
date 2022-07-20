86188d864d0a7c484715f8a19dfa70a2ba99e6a4
syafiqmk <syafiqmuh.k@gmail.com>
2022-07-20 09:31:01 +0800

    update show instructor

--- resources/views/admin/instructor/show.blade.php
+++ resources/views/admin/instructor/show.blade.php
@@ -5,5 +5,29 @@
 @endsection
 
 @section('content')
-    <img src="{{ asset('/storage/'. $user->photo) }}" alt="" class="img-fluid col-sm-7">
+    <img src="{{ asset('/storage/'. $user->photo) }}" alt="" class="img-fluid col-sm-7 mb-4">
+
+    <table class="table">
+        <tr>
+            <td>Name</td>
+            <td>:</td>
+            <td>{{ $user->name }}</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>:</td>
+            <td>{{ $user->email }}</td>
+        </tr>
+        <tr>
+            <td>Status</td>
+            <td>:</td>
+            <td>
+                @if ($user->status == 'ready')
+                    <p class="btn btn-success">Ready</p>
+                @else
+                    <p class="btn btn-danger">Not Ready</p>
+                @endif
+            </td>
+        </tr>
+    </table>
 @endsection
