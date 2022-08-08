96fd9f597c3e39d75203958a2737e196470947b8
syafiqmk <syafiqmuh.k@gmail.com>
2022-08-08 09:42:53 +0800

    create auth api

---
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class ApiAuthController extends Controller
+{
+    // Register
+    public function register(Request $request) {
+        $credentials = $request->validate([
+            'name' => 'required|min:3|max:100',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        $auth = User::create([
+            'name' => ucwords($credentials['name']),
+            'email' => $credentials['email'],
+            'password' => bcrypt($credentials['password'])
+        ]);
+
+        if($auth) {
+            return response([
+                'message' => 'Account created!',
+                'auth' => $auth,
+                'token' => $auth->createToken('kursus_mengemudi')->plainTextToken
+            ], 200);
+        } else {
+            return response([
+                'message' => 'Account fail to create!'
+            ]);
+        }
+    }
+
+    // Login
+    public function login(Request $request) {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $auth = User::where('email', $credentials['email'])->firstOrFail();
+
+        if ($auth) {
+            return response([
+                'message' => 'You are logged in!',
+                'auth' => $auth,
+                'token' => $auth->createToken('kursus_mengemudi')->plainTextToken
+            ]);
+        } else {
+            return response([
+                'message' => 'Fail to log in!'
+            ], 200);
+        }
+    }
+
+    // Profile
+    public function profile() {
+        return response([
+            'auth' => auth()->user()
+        ], 200);
+    }
+
+    // Logout
+    public function logout() {
+        auth()->user()->tokens()->delete();
+
+        return response([
+            'message' => 'You are logged out!'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Authorization
+Route::post('/register', [ApiAuthController::class, 'register']);
+Route::post('/login', [ApiAuthController::class, 'login']);
+
+// Transmission
+Route::get('/transmission', [ApiTransmissionController::Class, 'show']);
+
+// Route that must be authorized
+Route::middleware('auth:sanctum')->group(function() {
+    // Authorization
+    Route::get('/profile', [ApiAuthController::class, 'profile']);
+    Route::post('/logout', [ApiAuthController::class, 'logout']);
+});
+
+
+Route::get('/test', function() {
+    return response([
+        'message' => 'Hello World!'
+    ]);
 });
