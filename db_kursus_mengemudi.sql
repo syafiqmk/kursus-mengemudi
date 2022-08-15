ad928b9e442a9807f6608e34f7695666a8402d1d
syafiqmk <syafiqmuh.k@gmail.com>
2022-08-15 08:23:16 +0800

    update db

--- db_kursus_mengemudi.sql
+++ db_kursus_mengemudi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2022 at 04:56 AM
+-- Generation Time: Aug 15, 2022 at 02:19 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -34,6 +34,13 @@ CREATE TABLE `brands` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Daihatsu', '2022-08-07 16:54:17', '2022-08-07 16:54:17');
+
 -- --------------------------------------------------------
 
 --
@@ -46,30 +53,74 @@ CREATE TABLE `cars` (
   `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `engine_capacity` int(11) NOT NULL,
-  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('ready','not ready') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` bigint(20) DEFAULT NULL,
   `brand_id` bigint(20) UNSIGNED NOT NULL,
+  `student_id` bigint(20) UNSIGNED DEFAULT NULL,
   `transmission_id` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`id`, `registration_number`, `image`, `name`, `engine_capacity`, `status`, `price`, `brand_id`, `student_id`, `transmission_id`, `created_at`, `updated_at`) VALUES
+(1, 'KT 1234 ABC', 'images/car/x1MoJcNpmWprj8ZxeR2qZI2gIVv1Xm8nu1fMxuDs.jpg', 'Xenia', 1300, 'ready', NULL, 1, 2, 1, '2022-08-07 16:54:46', '2022-08-11 17:14:46'),
+(2, 'KT 123 XYZ', 'images/car/ebIDBikC78hVGKKLPUurTBSU5wDD0kqpJl1UV4fS.jpg', 'Xenia', 1500, 'ready', 800000, 1, NULL, 2, '2022-08-07 16:56:06', '2022-08-09 17:14:12'),
+(4, 'KT 2 ABC', 'images/car/xh2Bki5I8R4SP2v6QYGHk8UCHBpxFZYL1s407QxR.jpg', 'Xenia', 1500, 'ready', 800000, 1, NULL, 1, '2022-08-08 17:54:56', '2022-08-08 17:54:56');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `enrolls`
+-- Table structure for table `courses`
 --
 
-CREATE TABLE `enrolls` (
+CREATE TABLE `courses` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `user_id` bigint(20) UNSIGNED NOT NULL,
-  `package_id` bigint(20) UNSIGNED NOT NULL,
+  `student_id` bigint(20) UNSIGNED NOT NULL,
   `car_id` bigint(20) UNSIGNED NOT NULL,
+  `package_id` bigint(20) UNSIGNED NOT NULL,
   `instructor_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `payment_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('enroll','wait','grant','finish') COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `student_id`, `car_id`, `package_id`, `instructor_id`, `payment_image`, `status`, `created_at`, `updated_at`) VALUES
+(1, 2, 1, 1, 3, 'images/payment/SvRjidK349LpsF9gjyhCSx5dssnaiPWKaBu0u6QE.jpg', 'finish', '2022-08-07 16:58:21', '2022-08-11 17:14:46'),
+(2, 2, 2, 1, 3, 'images/payment/SzowiHRf0dWckTzeUg6MGHt7YvVNcKwIkRAL7nMG.jpg', 'finish', '2022-08-09 17:14:12', '2022-08-10 18:03:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_details`
+--
+
+CREATE TABLE `course_details` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `course_id` bigint(20) UNSIGNED NOT NULL,
+  `detail` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `course_details`
+--
+
+INSERT INTO `course_details` (`id`, `course_id`, `detail`, `created_at`, `updated_at`) VALUES
+(1, 2, 'contoh detail 1', '2022-08-10 17:43:12', '2022-08-10 17:43:12'),
+(2, 2, 'contoh detail 2', '2022-08-10 17:43:22', '2022-08-10 17:43:22'),
+(3, 2, 'Course Finished!', '2022-08-10 18:03:34', '2022-08-10 18:03:34'),
+(4, 1, 'test', '2022-08-11 17:07:17', '2022-08-11 17:07:17'),
+(5, 1, 'Course Finished', '2022-08-11 17:14:46', '2022-08-11 17:14:46');
 
 -- --------------------------------------------------------
 
@@ -108,11 +159,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2022_07_19_010754_create_cars_table', 1),
-(6, '2022_07_19_010809_create_brands_table', 1),
-(7, '2022_07_19_010828_create_transmissions_table', 1),
+(5, '2022_07_19_010809_create_brands_table', 1),
+(6, '2022_07_19_010809_create_transmissions_table', 1),
+(7, '2022_07_19_010810_create_cars_table', 1),
 (8, '2022_07_20_013142_create_packages_table', 1),
-(9, '2022_07_20_031146_create_enrolls_table', 1);
+(9, '2022_08_06_094031_create_courses_table', 1),
+(10, '2022_08_07_062925_create_course_details_table', 1);
 
 -- --------------------------------------------------------
 
@@ -125,10 +177,16 @@ CREATE TABLE `packages` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `detail` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` bigint(20) NOT NULL,
-  `transmission_id` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `packages`
+--
+
+INSERT INTO `packages` (`id`, `name`, `detail`, `price`, `created_at`, `updated_at`) VALUES
+(1, 'Paket Pemula', 'Pilih paket ini jika anda tidak tau cara mengemudi.', 2500000, '2022-08-07 16:57:25', '2022-08-07 16:57:25');
 
 -- --------------------------------------------------------
 
@@ -160,6 +218,13 @@ CREATE TABLE `personal_access_tokens` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `personal_access_tokens`
+--
+
+INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `created_at`, `updated_at`) VALUES
+(11, 'App\\Models\\User', 1, 'kursus_mengemudi', '737b54adf20a699e4a70040468a47241aef91e0c3afa74477be1594e0e968143', '[\"*\"]', '2022-08-11 16:43:35', '2022-08-10 21:25:59', '2022-08-11 16:43:35');
+
 -- --------------------------------------------------------
 
 --
@@ -178,8 +243,9 @@ CREATE TABLE `transmissions` (
 --
 
 INSERT INTO `transmissions` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Automatic', '2022-07-20 18:35:01', '2022-07-20 18:35:01'),
-(2, 'Manual', '2022-07-20 18:35:01', '2022-07-20 18:35:01');
+(1, 'Automatic', '2022-08-07 16:54:17', '2022-08-07 16:54:17'),
+(2, 'Manual', '2022-08-07 16:54:17', '2022-08-07 16:54:17'),
+(3, 'Electric', '2022-08-07 16:54:17', '2022-08-07 16:54:17');
 
 -- --------------------------------------------------------
 
@@ -194,7 +260,7 @@ CREATE TABLE `users` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('admin','student','instructor') COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -206,8 +272,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `photo`, `email`, `email_verified_at`, `password`, `role`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', NULL, 'admin@mail.com', NULL, '$2y$10$Q/TzwJlfG5oVTRfNsDmEmuodiuwlcraWUpdORjCZnn/mQw2281bFi', 'admin', NULL, NULL, '2022-07-20 18:35:01', '2022-07-20 18:35:01'),
-(2, 'Syafiq Muhammad Kahfi', 'images/student/xhR4QN3Gu3jB9RD0nCYFyNOXn0ecnMZDxX4jU2eO.jpg', 'syafiq@mail.com', NULL, '$2y$10$gg4kiWGclRQli.WmoXkj0OMLCUyRDKi7Y0lTIiyE4JQ2qPskTw9jG', 'student', NULL, NULL, '2022-07-20 18:35:19', '2022-07-20 18:52:42');
+(1, 'Admin', NULL, 'admin@mail.com', NULL, '$2y$10$eKg5ddrjHdibDp0UGdblmeO2VDgwjMaLwU.kbT5FSq6X5gd.F5XuG', 'admin', NULL, NULL, '2022-08-07 16:54:17', '2022-08-07 16:54:17'),
+(2, 'Syafiq Muhammad Kahfi', 'images/student/o8o7l4Vxl01o0SohsfVisCNxLDW5kAxVUHnxsxQ4.jpg', 'syafiq@mail.com', NULL, '$2y$10$kbZsUR9x1u9XjKFGJ8Tfu.QdiWOQ7eCVRmkJqdkOQ5GV4qmsyXhxC', 'student', NULL, NULL, '2022-08-07 16:54:17', '2022-08-10 16:49:11'),
+(3, 'Ken Block', 'images/instructor/guyXxGteq9qEJVORZGaZVgfjkJQaPJ5x5efQeKzn.jpg', 'block@mail.com', NULL, '$2y$10$6OV0S6Ii0cYR6FX1/ZuNG.Ecys.0L7svSYM5cHVPD0Q52JH63Hz7q', 'instructor', 'ready', NULL, '2022-08-07 16:54:17', '2022-08-11 17:14:46');
 
 --
 -- Indexes for dumped tables
@@ -224,12 +291,25 @@ ALTER TABLE `brands`
 --
 ALTER TABLE `cars`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `cars_registration_number_unique` (`registration_number`);
+  ADD UNIQUE KEY `cars_registration_number_unique` (`registration_number`),
+  ADD KEY `cars_brand_id_foreign` (`brand_id`),
+  ADD KEY `cars_student_id_foreign` (`student_id`),
+  ADD KEY `cars_transmission_id_foreign` (`transmission_id`);
 
 --
--- Indexes for table `enrolls`
+-- Indexes for table `courses`
 --
-ALTER TABLE `enrolls`
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `courses_student_id_foreign` (`student_id`),
+  ADD KEY `courses_car_id_foreign` (`car_id`),
+  ADD KEY `courses_package_id_foreign` (`package_id`),
+  ADD KEY `courses_instructor_id_foreign` (`instructor_id`);
+
+--
+-- Indexes for table `course_details`
+--
+ALTER TABLE `course_details`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -286,19 +366,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `enrolls`
+-- AUTO_INCREMENT for table `courses`
 --
-ALTER TABLE `enrolls`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `courses`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `course_details`
+--
+ALTER TABLE `course_details`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -310,31 +396,52 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `packages`
 --
 ALTER TABLE `packages`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `transmissions`
 --
 ALTER TABLE `transmissions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cars`
+--
+ALTER TABLE `cars`
+  ADD CONSTRAINT `cars_brand_id_foreign` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `cars_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `cars_transmission_id_foreign` FOREIGN KEY (`transmission_id`) REFERENCES `transmissions` (`id`);
+
+--
+-- Constraints for table `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `courses_car_id_foreign` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `courses_instructor_id_foreign` FOREIGN KEY (`instructor_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `courses_package_id_foreign` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `courses_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
